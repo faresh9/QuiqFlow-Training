deb3f16a284973e005164cd1eb582045f27f1b24
fares haroun <far0213025@ju.edu.jo>
2025-04-09 14:32:40 +0300

    Add menu options for user interaction after greeting

--- pre-training-assignment/script.js
+++ pre-training-assignment/script.js
@@ -9,5 +9,16 @@ console.log("hey there! welcome to this little app");
 
 rl.question('what should i call you? ', (name) => {
   console.log(`nice to meet you, ${name}!`);
-  rl.close();
+
+  const menu = `
+  what would you like to do?
+  1. see today's date
+  2. get a motivational quote
+  3. exit
+  `;
+
+  rl.question(menu, (choice) => {
+    console.log(`you selected option ${choice}, but functionality coming soon!`);
+    rl.close();
+  });
 });
