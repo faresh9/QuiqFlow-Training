89b24af784313ec29a12fb317e4c628aa7ed65f4
fares haroun <far0213025@ju.edu.jo>
2025-04-16 16:54:07 +0300

    feat: Add coinCombination function for calculating coin combinations and findLongestCollatz function for longest Collatz sequence

---
+++ src/Daily-Problem-Solving/Day-2/script.js
@@ -0,0 +1,12 @@
+function coinCombination(target) {
+    let coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    let count = Array(target + 1).fill(0);
+    count[0] = 1;
+    for (let c = 0; c < coins.length; c++) {
+      for (let i = coins[c]; i <= target; i++) {
+        count[i] += count[i - coins[c]];
+      }
+    }
+    return count[target];
+  }
+  console.log(coinCombination(200));
---
+++ src/Daily-Problem-Solving/Day-3/script.js
@@ -0,0 +1,22 @@
+function findLongestCollatz(limit) {
+    let maxLength = 0;
+    let numberWithMax = 0;
+    for (let i = 1; i < limit; i++) {
+      let n = i;
+      let count = 1;
+      while (n != 1) {
+        if (n % 2 == 0) {
+          n = n / 2;
+        } else {
+          n = 3 * n + 1;
+        }
+        count = count + 1;
+      }
+      if (count > maxLength) {
+        maxLength = count;
+        numberWithMax = i;
+      }
+    }
+    return numberWithMax;
+  }
+  console.log("Longest chain under 1 million starts at: " + findLongestCollatz(1000000));
